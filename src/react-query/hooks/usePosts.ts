f392c43294fe22b262d998b5f0d4c97f46bf2a4a
Slienfore <Slienfore@163.com>
2024-05-17 22:39:21 +0800

    Parameterized Queries of Selector

--- src/react-query/hooks/usePosts.ts
+++ src/react-query/hooks/usePosts.ts
@@ -8,12 +8,19 @@ interface Post {
   userId: number;
 }
 
-const usePosts = () =>
+const usePosts = (userId: number | undefined) =>
   useQuery<Post[], Error>({
-    queryKey: ["posts"],
+    // queryKey: 由静态和动态组成 -> "users","posts" 是静态, userId 是动态
+    // 用于唯一标识特定查询, 能够区分和管理不同的请求
+    // 如果 userId 发生变化, 那么将会重新刷新, 类似 useEffect 的 dependence
+    queryKey: userId ? ["users", userId, "posts"] : ["posts"],
     queryFn: () =>
       axios
-        .get("https://jsonplaceholder.typicode.com/posts")
+        .get("https://jsonplaceholder.typicode.com/posts", {
+          params: {
+            userId,
+          },
+        })
         .then((res) => res.data),
     staleTime: 1 * 60 * 1000, // expired in 1 minute
   });
