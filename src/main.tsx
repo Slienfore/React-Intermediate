f45494a541245f4c345130ba9452c443669804a6
Slienfore <Slienfore@163.com>
2024-05-17 21:58:16 +0800

    Customizing Query Settings

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,19 @@ import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import App from "./App";
 import "./index.css";
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: 3, // retry times
+      cacheTime: 300_000, // cache expired time -> 5 minutes = (((300,000) / 1000) / 60)
+      staleTime: 10_000, // the time of the data is fresh -> 10s
+      // refetch data -> (如果缓存中数据是旧的, 那么Query将会向服务器请求新的数据, 然后替换组件中的数据, 且更新缓存中的数据)
+      refetchOnWindowFocus: false,
+      refetchOnReconnect: false,
+      refetchOnMount: false,
+    },
+  },
+});
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
