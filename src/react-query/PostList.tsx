ee0e4abfe8051406e45637fff5578563b89aae0e
Slienfore <Slienfore@163.com>
2024-05-18 11:13:07 +0800

    Infinite Query

--- src/react-query/PostList.tsx
+++ src/react-query/PostList.tsx
@@ -1,43 +1,38 @@
-import { useState } from "react";
+import React from "react";
 import usePosts from "./hooks/usePosts";
 
 const PostList = () => {
   const pageSize = 10; // just make it const
-  const [page, setPage] = useState(1);
 
-  const {
-    data: posts,
-    error,
-    isLoading,
-  } = usePosts({
-    page,
-    pageSize,
-  });
+  const { data, error, isLoading, fetchNextPage, isFetchingNextPage } =
+    usePosts({ pageSize });
 
   if (isLoading) return <p>Loading...</p>;
-
   if (error) return <p>{error.message}</p>;
-
   return (
     <>
       <ul className="list-group">
-        {posts?.map((post) => (
-          <li key={post.id} className="list-group-item">
-            {post.title}
-          </li>
+        {/* 
+            无限查询的数据是按 "页" 进行组织的, 每一页的数据都是一个数组 【Array(length), Array(length)】
+            渲染时: 遍历每一页数据, 再遍历每一页中的具体内容
+         */}
+        {data.pages.map((page, index) => (
+          <React.Fragment key={index}>
+            {page.map((post) => (
+              <li key={post.id} className="list-group-item">
+                {post.title}
+              </li>
+            ))}
+          </React.Fragment>
         ))}
       </ul>
 
       <button
-        onClick={() => setPage(page - 1)}
-        disabled={page === 1}
-        className="btn btn-primary my-3"
+        onClick={() => fetchNextPage()}
+        disabled={isFetchingNextPage}
+        className="btn btn-primary my-3 ms-1"
       >
-        Previous
-      </button>
-
-      <button onClick={() => setPage(page + 1)} className="btn btn-primary my-3 ms-1">
-        Next
+        {isFetchingNextPage ? "Loading..." : "Load More"}
       </button>
     </>
   );
--- src/react-query/hooks/usePosts.ts
+++ src/react-query/hooks/usePosts.ts
@@ -1,5 +1,5 @@
-import { useQuery } from "@tanstack/react-query";
-import axios from "axios";
+import { useInfiniteQuery } from "@tanstack/react-query";
+import axios, { all } from "axios";
 
 interface Post {
   id: number;
@@ -9,24 +9,28 @@ interface Post {
 }
 
 interface PostQuery {
-  page: number;
   pageSize: number;
 }
 
 const usePosts = (query: PostQuery) =>
-  useQuery<Post[], Error>({
+  useInfiniteQuery<Post[], Error>({
     queryKey: ["posts", query],
-    queryFn: () =>
+    queryFn: ({ pageParam = 1 }) =>
       axios
         .get("https://jsonplaceholder.typicode.com/posts", {
           params: {
-            _start: (query.page - 1) * query.pageSize, // 分页查询, 每一页最多有多少条发表笔记
+            _start: (pageParam - 1) * query.pageSize, // 分页查询, 每一页最多有多少条发表笔记
             _limit: query.pageSize,
           },
         })
         .then((res) => res.data),
     staleTime: 1 * 60 * 1000, // expired in 1 minute
-    keepPreviousData: true,// 为避免新数据加载时显示loading空白闪烁状态, React Query将会保持和显示当前的旧数据
+    keepPreviousData: true, // 为避免新数据加载时显示loading空白闪烁状态, React Query将会保持和显示当前的旧数据
+
+    getNextPageParam: (lastPage, allPages) => {
+      // allPages: 存储的是, 进行分页查询的每次数据 -> allPages[ Array(10), Array(10) ]
+      return lastPage.length > 0 ? allPages.length + 1 : undefined;
+    },
   });
 
 export default usePosts;
