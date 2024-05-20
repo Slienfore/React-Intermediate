26cc429643867ac334c1377af36856a2204f78fd
Slienfore <Slienfore@163.com>
2024-05-20 21:02:41 +0800

    Creating Tasks Provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,19 @@
-import { useReducer } from "react";
 import "./App.css";
 import AuthProvider from "./state-management/AuthProvider";
 import HomePage from "./state-management/HomePage";
 import NavBar from "./state-management/NavBar";
-import TasksContext from "./state-management/contexts/tasksContext";
-import tasksReducer from "./state-management/reducers/tasksReducer";
+import TasksProvider from "./state-management/TasksProvider";
 
 function App() {
-  const [tasks, tasksDispatch] = useReducer(tasksReducer, []);
-
   return (
     <>
       {/* 自定义 Context Provider */}
       <AuthProvider>
-        <TasksContext.Provider value={{ tasks, dispatch: tasksDispatch }}>
+        {/* 自定义 Context Provider */}
+        <TasksProvider>
           <NavBar></NavBar>
           <HomePage></HomePage>
-        </TasksContext.Provider>
+        </TasksProvider>
       </AuthProvider>
     </>
   );
