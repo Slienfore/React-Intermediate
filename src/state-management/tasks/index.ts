7bf693876d19a14f8837f892d6b3bb61adaa5612
Slienfore <Slienfore@163.com>
2024-05-20 22:31:18 +0800

    Organizing Code for Scalability and Maintainability

---
+++ src/state-management/tasks/index.ts
@@ -0,0 +1,8 @@
+/* import TasksProvider from "./TasksProvider";
+export { TasksProvider }; */
+
+/* import TaskList from "./TaskList";
+export { TaskList }; */
+
+export { default as TasksProvider } from "./TasksProvider";
+export { default as TaskList } from "./TaskList";
