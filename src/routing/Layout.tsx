d6aec40eb0e4e406304593ce772387f08309e2de
Slienfore <Slienfore@163.com>
2024-05-21 16:10:03 +0800

    Nested Routes

--- src/routing/Layout.tsx
+++ src/routing/Layout.tsx
@@ -1,10 +1,15 @@
-import NavBar from './NavBar';
+import { Outlet } from "react-router-dom";
+import NavBar from "./NavBar";
 
 const Layout = () => {
   return (
     <>
       <NavBar />
-      <div id="main"></div>
+
+      <div id="main">
+        {/* 不同的路由组件将会呈现在这里 */}
+        <Outlet></Outlet>
+      </div>
     </>
   );
 };
--- src/routing/routes.tsx
+++ src/routing/routes.tsx
@@ -2,11 +2,22 @@ import { createBrowserRouter } from "react-router-dom";
 import HomePage from "./HomePage";
 import UserDetailPage from "./UserDetailPage";
 import UserListPage from "./UserListPage";
+import Layout from "./Layout";
 
 const router = createBrowserRouter([
-  { path: "/", element: <HomePage /> },
-  { path: "/users", element: <UserListPage /> },
-  { path: "/user/:id", element: <UserDetailPage /> },
+  {
+    path: "/",
+    element: <Layout />,
+
+    children: [
+      // index => 默认呈现组件
+      { index: true, element: <HomePage /> },
+
+      { path: "users", element: <UserListPage /> },
+      { path: "users/:id", element: <UserDetailPage /> },
+    ],
+    
+  },
 ]);
 
 export default router;
