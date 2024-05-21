138f10c0aaf9554e710c68d0c194707705183920
Slienfore <Slienfore@163.com>
2024-05-21 15:58:12 +0800

    Getting Data about the Current Route

--- src/routing/UserDetailPage.tsx
+++ src/routing/UserDetailPage.tsx
@@ -1,4 +1,19 @@
+import { useLocation, useParams, useSearchParams } from "react-router-dom";
+
 const UserDetailPage = () => {
+  // Restful -> 参数
+  const params = useParams();
+  console.log(params);
+
+  // Query -> 参数
+  const [searchParams, setSearchParams] = useSearchParams();
+  console.log(searchParams.toString());
+  console.log(searchParams.get("name"), searchParams.get("age"));
+
+  // 路由位置
+  const location = useLocation();
+  console.log(location);
+
   return <p>User</p>;
 };
 
